2413befe0e861b40bfdf3362c595a3be01bb70c5
Martin Melka <martin.melka@rohea.com>
2025-02-03 08:49:43 +0100

    Fix version tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='toggltempo',
-    version='2.0.0',
+    version='2.1.1',
     url='https://github.com/melkamar/toggltempo',
     author='Martin Melka',
     author_email='martin.melka@gmail.com',
